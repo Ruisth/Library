98abe69e21deef4890f3fd1e83e3b255f18f027a
Ruisth <ruimfduarte94@gmail.com>
2024-11-12 20:45:35 +0000

    Added new endpoints to books

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -12,11 +12,57 @@ router.get("/", async (req, res) => {
     res.send(results).status(200);
 });
 
+// post books
+/*
+_id: 1
+title: "Unlocking Android"
+isbn: "1933988673"
+pageCount: 416
+publishedDate: 2009-04-01T07:00:00.000+00:00
+thumbnailUrl: "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/ableso…"
+shortDescription: "Unlocking Android: A Developer's Guide provides concise, hands-on inst…"
+longDescription: "Android is an open source mobile phone platform based on the Linux ope…"
+status: "PUBLISH"
+authors Array (3)
+0 "W. Frank Ableson"
+1 "Charlie Collins"
+2 "Robi Sen"
+categories Array (2)
+0 "Open Source"
+1 "Mobile"
+*/
+router.post("/", async (req, res) => {
+    const { title, isbn, pageCount, publishedDate, thumbnailUrl, shortDescription, longDescription, status, authors, categories } = req.body;
+
+    if (!title || !isbn || !pageCount || !publishedDate || !thumbnailUrl || !shortDescription || !longDescription || !status || !authors || !categories) {
+        return res.status(400).json({ error: 'Dados inválidos' });
+    }
+
+    const newBook = {
+        title,
+        isbn,
+        pageCount,
+        publishedDate,
+        thumbnailUrl,
+        shortDescription,
+        longDescription,
+        status,
+        authors,
+        categories
+    };
+
+    try {
+        const result = await db.collection('books').insertOne(newBook);
+        res.status(201).json(result.ops[0]);
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao inserir o livro.' });
+    }
+});
+
 // return a book by id
 router.get("/:id", async (req, res) => {
     const bookId = parseInt(req.params.id);
 
-    // Verifica se o bookId é um ObjectId válido
     if (!ObjectId.isValid(bookId)) {
         return res.status(400).json({ error: 'ID inválid: ' + bookId });
     }
@@ -34,6 +80,48 @@ router.get("/:id", async (req, res) => {
     }
 });
 
+
+// update a book by id
+
+
+
+
+
+
+// delete a book by id
+
+
+
+
+
+// get books with higher score with a limit
+
+
+
+
+
+// get books by total reviews
+
+
+
+
+// get books by stars
+
+
+
+
+// get books by year
+
+
+
+
+
+// get books with comments 
+
+
+
+
+// get books by job
 
 
 

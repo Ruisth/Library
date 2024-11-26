410626f48a989dec7e31ece5a1cb20abe47654cb
Ruisth <ruimfduarte94@gmail.com>
2024-11-26 18:34:22 +0000

    Initial commit for Frontend

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,13 @@ import books from "./routes/books.js";
 import users from "./routes/users.js";
 import comments from "./routes/comments.js";
 import livrarias from "./routes/livrarias.js";
+import cors from 'cors';
 
 const app = express()
 const port = 3000
 
 app.use(express.json());
+app.use(cors());
 
 // Load the /books routes
 app.use("/books", books);

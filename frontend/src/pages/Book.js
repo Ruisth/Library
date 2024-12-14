3590a8ff11e7616d2b25a363b7cec76bef2549de
Ruisth <ruimfduarte94@gmail.com>
2024-12-14 16:42:04 +0000

    removed unused imports

--- frontend/src/pages/Book.js
+++ frontend/src/pages/Book.js
@@ -1,13 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { openContractCall } from '@stacks/connect';
-import {
-  bufferCV,
-} from '@stacks/transactions';
 import { utf8ToBytes } from '@stacks/common';
-import { userSession } from '../auth';
 const bytes = utf8ToBytes('foo');
-const bufCV = bufferCV(bytes);
 
 
 export default function App() {
